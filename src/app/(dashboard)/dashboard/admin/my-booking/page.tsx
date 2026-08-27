06b4ca4c42eb2d1014f4069eff3543c2a39461a1
codewitharafat1 <arafatnill2.0@gmail.com>
2026-08-27 19:25:15 +0600

    fixed deploy issue

--- src/app/(dashboard)/dashboard/admin/my-booking/page.tsx
+++ src/app/(dashboard)/dashboard/admin/my-booking/page.tsx
@@ -1,13 +1,19 @@
 import MyBookingList from "../../_components/MyBookingList";
 import { getCustomerBookings } from "../../_actions/bookingActions";
 import EmptyBookingState from "@/components/dashboard/EmptyBookingState";
+import { SearchProp } from "@/lib/types";
 
-const MyBooking = async () => {
-  const response = await getCustomerBookings();
+const MyBooking = async ({ searchParams }: SearchProp) => {
+  const query = await searchParams;
+  const response = await getCustomerBookings({ query });
   const bookingsArray = response?.data || [];
 
   if (!response?.success || bookingsArray.length === 0) {
-    return <div className="p-10"><EmptyBookingState/></div>
+    return (
+      <div className="p-10">
+        <EmptyBookingState />
+      </div>
+    );
   }
 
   return (
